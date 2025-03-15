84ca06755fb12085b97eb200d2dd6078c2a93e92
Santiago Alvarez <s.alvarez1123@uniandes.edu.co>
2025-03-15 10:57:05 -0500

    1

---
+++ src/main/java/uniandes/edu/co/proyecto/modelo/IPS.java
@@ -0,0 +1,50 @@
+package uniandes.edu.co.proyecto.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="IPS")
+public abstract class IPS {
+    
+    @Id
+    private int Nit;
+    private char nombre;
+    private char direccion;
+    private char telefono;
+
+    public IPS(int Nit, char nombre, char direccion, char telefono){
+        this.Nit=Nit;
+        this.nombre=nombre;
+        this.direccion=direccion;
+        this.telefono=telefono;
+    }
+    public IPS(){;}
+
+    public int getNit() {
+        return Nit;
+    }
+    public void setNit(int nit) {
+        Nit = nit;
+    }
+    public char getNombre() {
+        return nombre;
+    }
+    public void setNombre(char nombre) {
+        this.nombre = nombre;
+    }
+    public char getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(char direccion) {
+        this.direccion = direccion;
+    }
+    public char getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(char telefono) {
+        this.telefono = telefono;
+    }
+
+}
---
+++ src/main/java/uniandes/edu/co/proyecto/modelo/Medico.java
@@ -0,0 +1,74 @@
+package uniandes.edu.co.proyecto.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="MEDICO")
+public class Medico {
+    
+    @Id
+    private int NumeroDocumento;
+    private char TipoDocumento;
+    private char Especialidad;
+    private char RegistroMedico;
+    
+    @ManyToOne
+    @JoinColumn(name = "IPS", referencedColumnName = "Nit")
+    private int IPS;
+
+    public Medico(){;}
+
+    public Medico(int numeroDocumento, char tipoDocumento, char especialidad, char registroMedico, int iPS) {
+        NumeroDocumento = numeroDocumento;
+        TipoDocumento = tipoDocumento;
+        Especialidad = especialidad;
+        RegistroMedico = registroMedico;
+        IPS = iPS;
+    }
+
+    public int getNumeroDocumento() {
+        return NumeroDocumento;
+    }
+
+    public void setNumeroDocumento(int numeroDocumento) {
+        NumeroDocumento = numeroDocumento;
+    }
+
+    public char getTipoDocumento() {
+        return TipoDocumento;
+    }
+
+    public void setTipoDocumento(char tipoDocumento) {
+        TipoDocumento = tipoDocumento;
+    }
+
+    public char getEspecialidad() {
+        return Especialidad;
+    }
+
+    public void setEspecialidad(char especialidad) {
+        Especialidad = especialidad;
+    }
+
+    public char getRegistroMedico() {
+        return RegistroMedico;
+    }
+
+    public void setRegistroMedico(char registroMedico) {
+        RegistroMedico = registroMedico;
+    }
+
+    public int getIPS() {
+        return IPS;
+    }
+
+    public void setIPS(int iPS) {
+        IPS = iPS;
+    }
+
+    
+}
