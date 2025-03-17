055aef4876cc2c2f8d0fc14672d70c3663285396
Santiago Alvarez <s.alvarez1123@uniandes.edu.co>
2025-03-17 17:49:26 -0500

    Penultimo

---
+++ src/main/java/uniandes/edu/co/proyecto/controller/ControllerCitaMedica.java
@@ -0,0 +1,39 @@
+package uniandes.edu.co.proyecto.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import uniandes.edu.co.proyecto.modelo.CitaMedica;
+import uniandes.edu.co.proyecto.repositorio.CitaMedicaRepository;
+
+
+@Controller
+public class ControllerCitaMedica {
+
+    @Autowired
+    private  CitaMedicaRepository citaRepository;
+
+    @GetMapping("/CitasMedicas/new")
+    public String  CitaMedicaForm(Model model){
+        model.addAttribute("Servicio", new CitaMedica());
+        return "CitaMedicaNuevo";
+    }
+
+    @PostMapping("/CitasMedicas/new/save")
+    public String CitaMedicaGuardar(@ModelAttribute CitaMedica citaMedica) {
+        citaRepository.agregarCita(citaMedica.getFecha(), citaMedica.getHora(), citaMedica.getEstado(), citaMedica.getMedico().getNumeroDocumento(), citaMedica.getAfiliado().getNumeroDocumento());
+        return "redirect:/CitasMedicas/new";
+    }
+
+    @GetMapping("/CitaMedica")
+    public String getCitas4(Model model, @ModelAttribute CitaMedica citaChequear) {
+        model.addAttribute("CitasMedicas", citaRepository.consultarDisponibilidad(citaChequear.getFecha()));
+        return "CitasMedicas";
+    }
+    
+    
+}
---
+++ src/main/java/uniandes/edu/co/proyecto/controller/ControllerOrden.java
@@ -0,0 +1,29 @@
+package uniandes.edu.co.proyecto.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import uniandes.edu.co.proyecto.modelo.Orden;
+import uniandes.edu.co.proyecto.repositorio.OrdenRepository;
+
+@Controller
+public class ControllerOrden {
+
+    @Autowired
+    private  OrdenRepository ordenRepository;
+
+    @GetMapping("/Orden/new")
+    public String  OrdenForm(Model model){
+        model.addAttribute("Orden", new Orden());
+        return "OrdenNuevo";
+    }
+
+    @PostMapping("/Orden/new/save")
+    public String OrdenGuardar(@ModelAttribute Orden orden) {
+        ordenRepository.registrarOrden(orden.getFecha(), orden.getEstado(), orden.getMedico().getNumeroDocumento(), orden.getAfiliado().getNumeroDocumento());
+        return "redirect:/Orden/new";
+    }
+}
